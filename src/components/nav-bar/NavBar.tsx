1cb46c6d0af9d6118f27b715e6626f1217637de1
Kenny Davis <kennethdavis391@gmail.com>
2025-02-15 08:30:19 -0500

    updated navbar layout and menu items

--- src/components/nav-bar/NavBar.tsx
+++ src/components/nav-bar/NavBar.tsx
@@ -1,19 +1,67 @@
-import { Divider } from '@mui/material';
+import { Card, Divider, MenuItem, MenuList } from '@mui/material';
 import NavBarButton from './nav-bar-button/NavBarButton';
 import './NavBar.css';
+import React from 'react';
+
+type NavButton = {
+    title: string,
+    subMenu?: string[];
+};
 
 const NavBar = () => {
-    const navButtons: string[] = [
-        "Meal Plans",
-        "Grocery List",
-        "Recipes"
+    const navButtons: NavButton[] = [
+        {
+            title: "Meal Plans",
+            subMenu: [
+                "Current",
+                "View All",
+                "Create"
+            ]
+        },
+        {
+            title: "Recipes",
+            subMenu: [
+                "View All",
+                "Create"
+            ]
+        },
+        {
+            title: "Grocery Lists",
+            subMenu: [
+                "Current",
+                "View All"
+            ]
+        }
     ];
+    const [menuIndex, setMenuIndex] = React.useState<number>(-1);
+
     return <div className='nav-bar-container'>
         <div className='max-page-content'>
-            <h1 className='nav-bar-app-title'>App Title</h1>
-            <div className='nav-button-container'>
-                {navButtons.map((navButton: string) => <NavBarButton text={navButton} />)}
+            <div className='nav-bar-title-container'>
+                <h1 className='nav-bar-app-title'>Plan & Plate</h1>
             </div>
+            <div className='nav-button-list-container'>
+                {navButtons.map((navButton: NavButton, index: number) =>
+                    <span
+                        className='nav-button-container'
+                        key={index}
+                        onMouseEnter={() => { setMenuIndex(index); }}
+                        onMouseLeave={() => { setMenuIndex(-1); }}>
+                        <NavBarButton text={navButton.title} />
+                        {navButton.subMenu?.length && <Card sx={{
+                            backgroundColor: 'var(--card-color)',
+                            position: 'fixed',
+                            visibility: menuIndex === index ? 'visible' : 'hidden',
+                            opacity: menuIndex === index ? '1' : '0',
+                            transition: 'opacity .5s'
+                        }}>
+                            <MenuList>
+                                {navButton.subMenu.map((text: string, index: number) => <MenuItem key={index}>{text}</MenuItem>)}
+                            </MenuList>
+                        </Card>}
+                    </span>)}
+            </div>
+            <div className='flex-filler'></div>
         </div>
         <Divider />
     </div>;
--- src/components/nav-bar/nav-bar-button/NavBarButton.tsx
+++ src/components/nav-bar/nav-bar-button/NavBarButton.tsx
@@ -1,7 +1,10 @@
 import './NavBarButton.css';
 
 const NavBarButton = ({ text }: { text: string; }) => {
-    return <p className='hover-underline-animation nav-bar-button-text'>{text}</p>;
+
+    return <p className='nav-bar-button-text'>
+        {text}
+    </p>;
 };
 
 export default NavBarButton;
