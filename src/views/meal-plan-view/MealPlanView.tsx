e994ff873e53132cf340bd1d5c1a15c249129be0
Kenny Davis <kennethdavis391@gmail.com>
2025-08-17 11:34:20 -0400

    updated meal plan interaction to be a dropdown menu

--- src/views/meal-plan-view/MealPlanView.tsx
+++ src/views/meal-plan-view/MealPlanView.tsx
@@ -1,4 +1,4 @@
-import { IconButton, Skeleton } from "@mui/material";
+import { IconButton, ListItemIcon, ListItemText, Menu, MenuItem, Skeleton } from "@mui/material";
 import MealPlanDisplay from "../../components/meal-plan-display/MealPlanDisplay";
 import { MealPlan } from "../../models/MealPlan";
 import { getMealPlanById } from "../../services/mealPlanService";
@@ -6,10 +6,12 @@ import "./MealPlanView.css";
 import MealPlanDisplaySkeleton from "../../components/meal-plan-display/MealPlanDisplaySkeleton";
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router";
-import { Edit, LocalGroceryStore } from "@mui/icons-material";
+import { Edit, LocalGroceryStore, MoreVert } from "@mui/icons-material";
 
 const MealPlanView = () => {
   const [mealPlan, setMealPlan] = useState<MealPlan>();
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const menuOpen = Boolean(anchorEl);
 
   let { mealPlanId } = useParams();
 
@@ -20,6 +22,14 @@ const MealPlanView = () => {
         : setMealPlan(getMealPlanById(1));
     }, 250);
   }, [mealPlanId]);
+
+  const handleMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <div className="meal-plan-view-container">
@@ -33,30 +43,46 @@ const MealPlanView = () => {
           <h1 className="meal-plan-view-title">Meal Plan</h1>
           {mealPlan ? (
             <span className="meal-plan-view-edit-button meal-plan-view-flex-spacer">
-              <Link
-                to={`/mealplans/edit/${mealPlan.id}`}
-                className="no-link-style"
+              <IconButton
+                onClick={handleMenuOpen}
               >
-                <IconButton
-                  sx={(theme) => ({
-                    color: theme.palette.grey[500],
-                  })}
-                >
-                  <Edit />
-                </IconButton>
-              </Link>
-              <Link
-                to={`/grocerylist/mealplan/${mealPlan.id}`}
-                className="no-link-style"
+                <MoreVert />
+              </IconButton>
+              <Menu
+                anchorEl={anchorEl}
+                open={menuOpen}
+                onClose={handleMenuClose}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "right",
+                }}
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                sx={{'& .MuiMenu-paper': {backgroundColor: "var(--card-color)"}}}
               >
-                <IconButton
-                  sx={(theme) => ({
-                    color: theme.palette.grey[500],
-                  })}
+                <MenuItem
+                  component={Link}
+                  to={`/mealplans/edit/${mealPlan.id}`}
+                  onClick={handleMenuClose}
                 >
-                  <LocalGroceryStore />
-                </IconButton>
-              </Link>
+                  <ListItemIcon>
+                    <Edit fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText>Edit</ListItemText>
+                </MenuItem>
+                <MenuItem
+                  component={Link}
+                  to={`/grocerylist/mealplan/${mealPlan.id}`}
+                  onClick={handleMenuClose}
+                >
+                  <ListItemIcon>
+                    <LocalGroceryStore fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText>Grocery List</ListItemText>
+                </MenuItem>
+              </Menu>
             </span>
           ) : (
             <></>
