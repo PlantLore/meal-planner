0198b4fc4bc82ef8c0e4d4882e50f84d8e734a26
PlantLore <166308927+PlantLore@users.noreply.github.com>
2025-08-24 15:05:16 -0400

    feature/calorie-tracking (#16)
    * updated meal plan view to display total calories
    
    * updated meal plan upsert view to show total calories

--- src/components/meal-plan-display/MealPlanDisplay.tsx
+++ src/components/meal-plan-display/MealPlanDisplay.tsx
@@ -1,6 +1,9 @@
+import { LocalFireDepartmentOutlined } from "@mui/icons-material";
 import { MealPlan } from "../../models/MealPlan";
+import { MealPlanDay } from "../../models/MealPlanDay";
 import MealPlanDayDisplay from "./meal-plan-day-display/MealPlanDayDisplay";
 import "./MealPlanDisplay.css";
+import RecipeFact from "../recipe-card/recipe-fact/RecipeFact";
 
 const MealPlanDisplay = ({ mealPlan }: { mealPlan: MealPlan; }) => {
   const sortedMealPlanDays = [...mealPlan.mealPlanDays].sort((current, next) => current.day > next.day ? 1 : -1);
@@ -17,6 +20,16 @@ const MealPlanDisplay = ({ mealPlan }: { mealPlan: MealPlan; }) => {
     return mealPlanDay.day < today;
   });
 
+  const calculateCalories = (mealPlanDay: MealPlanDay): number => {
+    return mealPlanDay.meals.reduce(
+      (total, meal) =>
+        total + meal.mealRecipes.reduce(
+          (total, mealRecipe) =>
+            total + mealRecipe.recipe.calories, 0
+        )
+      , 0);
+  }
+
   return <>
     {currentMeals.map((mealPlanDay) =>
       <div className="meal-plan-day-display-container" key={mealPlanDay.id}>
@@ -24,14 +37,23 @@ const MealPlanDisplay = ({ mealPlan }: { mealPlan: MealPlan; }) => {
           {mealPlanDay.day.toLocaleDateString() === new Date().toLocaleDateString() ?
             "Today's Meals" :
             mealPlanDay.day.toLocaleDateString()
-          }</h1>
+          }
+          <span className="meal-plan-display-total-calories-container">
+            <RecipeFact tooltip={"Total Calories"} icon={<LocalFireDepartmentOutlined />} value={calculateCalories(mealPlanDay)}></RecipeFact>
+          </span>
+        </h1>
         <MealPlanDayDisplay mealPlanDay={mealPlanDay}></MealPlanDayDisplay>
       </div>
     )}
     <h1 className="meal-plan-display-previous-date-title meal-plan-display-date-title">Previous Meals</h1>
     {previousMeals.map((mealPlanDay) =>
       <div className="meal-plan-day-display-container" key={mealPlanDay.id}>
-        <h1 className="meal-plan-display-previous-date-title meal-plan-display-date-title"> {mealPlanDay.day.toLocaleDateString()} </h1>
+        <h1 className="meal-plan-display-previous-date-title meal-plan-display-date-title"> 
+          {mealPlanDay.day.toLocaleDateString()} 
+          <span className="meal-plan-display-total-calories-container">
+            <RecipeFact tooltip={"Total Calories"} icon={<LocalFireDepartmentOutlined />} value={calculateCalories(mealPlanDay)}></RecipeFact>
+          </span>
+        </h1>
         <MealPlanDayDisplay mealPlanDay={mealPlanDay}></MealPlanDayDisplay>
       </div>
     )}
